74494b1f2226311bed60c77a9c4b3a440724c8bc
jorgelondonoarias <jorgelondonoarias@gmail.com>
2025-03-05 19:56:28 +0100

    se añaden estructuras de metodos

--- Reservas__hotel/src/Vista/Consola.java
+++ Reservas__hotel/src/Vista/Consola.java
@@ -1,5 +1,33 @@
 package Vista;
 
+import java.util.Scanner;
+
 public class Consola {
 
+	public String pideDato(String testo) {
+		Scanner teclado = new Scanner(System.in);
+		return teclado.nextLine();
+	}
+	public int pideEntero(String texto) {
+		Scanner teclado = new Scanner(System.in);
+		return teclado.nextInt();
+	}
+	public void menu() {
+		
+	}
+	public void registro(){
+		
+	}
+	public void guardar_BBDD() {
+		
+	}
+	public void eliminarReserva() {
+		
+	}
+	public void modificarReserva() {
+		
+	}
+	public void consultarReserva() {
+		
+	}
 }
