5c15555da30168b568e3857de9c45885b64bb77d
jorgelondonoarias <jorgelondonoarias@gmail.com>
2025-03-06 13:26:36 +0100

    modificacion getter ans setters

--- Reservas__hotel/src/Modelo/Hotel.java
+++ Reservas__hotel/src/Modelo/Hotel.java
@@ -4,104 +4,51 @@ import static Modelo.Hotel.tipoHabitacion.INDIVIDUAL;
 
 public class Hotel {
 
-		private boolean reserva;
-		private int n_camas;
-		private boolean minibar;
 		private int n_piso;
-		private boolean desayuno;
-		private int dias;
-		private boolean sala_reuniones;
-		private boolean sala_banquetes;
-		private int n_habitacion;
-		public enum tipoHabitacion { INDIVIDUAL , MATRIMONIO , FAMILIA , DOBLE , SUIT};
-		public tipoHabitacion tipoHabitacion;
-		public boolean isReserva() {
-			return reserva;
-		}
-		public void setReserva(boolean reserva) {
-			this.reserva = reserva;
-		}
-		public int getN_camas() {
-			return n_camas;
-		}
-		public void setN_camas(int n_camas) {
-			this.n_camas = n_camas;
-		}
-		public boolean isMinibar() {
-			return minibar;
-		}
-		public void setMinibar(boolean minibar) {
-			this.minibar = minibar;
-		}
+		private int habitaciones;
+		private int sala_reuniones;
+		private int sala_banquetes;
+		private int terraza;
 		public int getN_piso() {
 			return n_piso;
 		}
 		public void setN_piso(int n_piso) {
 			this.n_piso = n_piso;
 		}
-		public boolean isDesayuno() {
-			return desayuno;
+		public int getHabitaciones() {
+			return habitaciones;
 		}
-		public void setDesayuno(boolean desayuno) {
-			this.desayuno = desayuno;
+		public void setHabitaciones(int habitaciones) {
+			this.habitaciones = habitaciones;
 		}
-		public int getDias() {
-			return dias;
-		}
-		public void setDias(int dias) {
-			this.dias = dias;
-		}
-		public boolean isSala_reuniones() {
+		public int getSala_reuniones() {
 			return sala_reuniones;
 		}
-		public void setSala_reuniones(boolean sala_reuniones) {
+		public void setSala_reuniones(int sala_reuniones) {
 			this.sala_reuniones = sala_reuniones;
 		}
-		public boolean isSala_banquetes() {
+		public int getSala_banquetes() {
 			return sala_banquetes;
 		}
-		public void setSala_banquetes(boolean sala_banquetes) {
+		public void setSala_banquetes(int sala_banquetes) {
 			this.sala_banquetes = sala_banquetes;
 		}
-		public int getN_habitacion() {
-			return n_habitacion;
+		public int getTerraza() {
+			return terraza;
 		}
-		public void setN_habitacion(int n_habitacion) {
-			this.n_habitacion = n_habitacion;
+		public void setTerraza(int terraza) {
+			this.terraza = terraza;
 		}
-		public tipoHabitacion getTipoHabitacion() {
-			return tipoHabitacion;
-		}
-		public void setTipoHabitacion(tipoHabitacion tipoHabitacion) {
-			this.tipoHabitacion = tipoHabitacion;
-		}
-		public Hotel(boolean reserva, int n_camas, boolean minibar, int n_piso, boolean desayuno, int dias,
-				 boolean sala_reuniones, boolean sala_banquetes, int n_habitacion,
-				Modelo.Hotel.tipoHabitacion tipoHabitacion) {
+		
+		public Hotel(int n_piso, int habitaciones, int sala_reuniones, int sala_banquetes, int terraza) {
 			super();
-			this.reserva = reserva;
-			this.n_camas = n_camas;
-			this.minibar = minibar;
 			this.n_piso = n_piso;
-			this.desayuno = desayuno;
-			this.dias = dias;
+			this.habitaciones = habitaciones;
 			this.sala_reuniones = sala_reuniones;
 			this.sala_banquetes = sala_banquetes;
-			this.n_habitacion = n_habitacion;
-			this.tipoHabitacion = tipoHabitacion;
-		}
-		public Hotel (){
-			this.reserva = true;
-			this.n_camas = 1;
-			this.minibar = false;
-			this.n_piso = 1;
-			this.desayuno = false;
-			this.dias = 1;
-			this.sala_reuniones = false;
-			this.sala_banquetes = false;
-			this.n_habitacion= 101;
-			this.tipoHabitacion=INDIVIDUAL;
-	
-		}
+			this.terraza = terraza;
+		}		
+
+		
 		
 }
