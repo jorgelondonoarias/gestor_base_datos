7f98fc91c45cdb8e83cdcf73167c7a7eb595da46
jorgelondonoarias <jorgelondonoarias@gmail.com>
2025-03-06 15:55:49 +0100

    modificacion de reserva

--- Reservas__hotel/src/Modelo/Hotel.java
+++ Reservas__hotel/src/Modelo/Hotel.java
@@ -8,47 +8,5 @@ public class Hotel {
 		private int habitaciones;
 		private int sala_reuniones;
 		private int sala_banquetes;
-		private int terraza;
-		public int getN_piso() {
-			return n_piso;
-		}
-		public void setN_piso(int n_piso) {
-			this.n_piso = n_piso;
-		}
-		public int getHabitaciones() {
-			return habitaciones;
-		}
-		public void setHabitaciones(int habitaciones) {
-			this.habitaciones = habitaciones;
-		}
-		public int getSala_reuniones() {
-			return sala_reuniones;
-		}
-		public void setSala_reuniones(int sala_reuniones) {
-			this.sala_reuniones = sala_reuniones;
-		}
-		public int getSala_banquetes() {
-			return sala_banquetes;
-		}
-		public void setSala_banquetes(int sala_banquetes) {
-			this.sala_banquetes = sala_banquetes;
-		}
-		public int getTerraza() {
-			return terraza;
-		}
-		public void setTerraza(int terraza) {
-			this.terraza = terraza;
-		}
-		
-		public Hotel(int n_piso, int habitaciones, int sala_reuniones, int sala_banquetes, int terraza) {
-			super();
-			this.n_piso = n_piso;
-			this.habitaciones = habitaciones;
-			this.sala_reuniones = sala_reuniones;
-			this.sala_banquetes = sala_banquetes;
-			this.terraza = terraza;
-		}		
-
-		
-		
+		private int terraza;		
 }
--- Reservas__hotel/src/Modelo/Reserva.java
+++ Reservas__hotel/src/Modelo/Reserva.java
@@ -10,64 +10,123 @@ public class Reserva {
 	private int personasReservas;
 	private boolean salaBanquetes;
 	private boolean boda;
+	private boolean terraza;
 	private ArrayList <Habitacion> habitacion = new ArrayList<>();
 	private ArrayList <Inquilino> inquilino = new ArrayList<>();
+	
+
 	
 	public int getIdReserva() {
 		return idReserva;
 	}
+
+
+
 	public void setIdReserva(int idReserva) {
 		this.idReserva = idReserva;
 	}
+
+
+
 	public boolean isSalaReuniones() {
 		return salaReuniones;
 	}
+
+
+
 	public void setSalaReuniones(boolean salaReuniones) {
 		this.salaReuniones = salaReuniones;
 	}
+
+
+
 	public int getPersonasReservas() {
 		return personasReservas;
 	}
+
+
+
 	public void setPersonasReservas(int personasReservas) {
 		this.personasReservas = personasReservas;
 	}
+
+
+
 	public boolean isSalaBanquetes() {
 		return salaBanquetes;
 	}
+
+
+
 	public void setSalaBanquetes(boolean salaBanquetes) {
 		this.salaBanquetes = salaBanquetes;
 	}
+
+
+
 	public boolean isBoda() {
 		return boda;
 	}
+
+
+
 	public void setBoda(boolean boda) {
 		this.boda = boda;
 	}
+
+
+
+	public boolean isTerraza() {
+		return terraza;
+	}
+
+
+
+	public void setTerraza(boolean terraza) {
+		this.terraza = terraza;
+	}
+
+
+
 	public ArrayList<Habitacion> getHabitacion() {
 		return habitacion;
 	}
+
+
+
 	public void setHabitacion(ArrayList<Habitacion> habitacion) {
 		this.habitacion = habitacion;
 	}
+
+
+
 	public ArrayList<Inquilino> getInquilino() {
 		return inquilino;
 	}
+
+
+
 	public void setInquilino(ArrayList<Inquilino> inquilino) {
 		this.inquilino = inquilino;
 	}
+
 	
+
 	public Reserva(int idReserva, boolean salaReuniones, int personasReservas, boolean salaBanquetes, boolean boda,
-			ArrayList<Habitacion> habitacion, ArrayList<Inquilino> inquilino) {
+			boolean terraza, ArrayList<Habitacion> habitacion, ArrayList<Inquilino> inquilino) {
 		super();
 		this.idReserva = idReserva;
 		this.salaReuniones = salaReuniones;
 		this.personasReservas = personasReservas;
 		this.salaBanquetes = salaBanquetes;
 		this.boda = boda;
+		this.terraza = terraza;
 		this.habitacion = habitacion;
 		this.inquilino = inquilino;
 	}
-	
+
+
+
 	public Reserva() {
 		super();
 		this.idReserva = 000;
@@ -75,6 +134,7 @@ public class Reserva {
 		this.personasReservas = 1;
 		this.salaBanquetes = false;
 		this.boda = false;
+		this.terraza = false;
 		this.habitacion = new ArrayList<Habitacion>();
 		this.inquilino = new ArrayList<Inquilino>();
 	}
