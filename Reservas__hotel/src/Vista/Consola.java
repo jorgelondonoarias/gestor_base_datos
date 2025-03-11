fa6da328e77cf7789dbdc9cf7b05d4d2264fe5f5
maagaa19 <jorgefpn06@gmail.com>
2025-03-11 16:33:21 +0100

    Update Consola.java
    Creacion de menu

--- Reservas__hotel/src/Vista/Consola.java
+++ Reservas__hotel/src/Vista/Consola.java
@@ -13,16 +13,46 @@ import Modelo.Terraza;
 
 public class Consola {
 
-	public String pideDato(String texto) {
+	public String pideCadena(String texto) {
 		Scanner teclado = new Scanner(System.in);
+		System.out.print(texto);
 		return teclado.nextLine();
 	}
 	public int pideEntero(String texto) {
 		Scanner teclado = new Scanner(System.in);
+		System.out.print(texto);
 		return teclado.nextInt();
 	}
 	public void menu() {
-		
+		Scanner teclado = new Scanner(System.in);
+		int opcion;
+		do {
+			System.out.println("Pixel Resort");
+			System.out.println("1. Reservas);
+			System.out.println("2. Consultar reservas");
+			System.out.println("3. Modificar reservas");
+			System.out.println("4. Eliminar reservas");
+			System.out.println("0. Bloquear");
+			opcion = teclado.nextInt();
+
+			switch(opcion) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 0:
+					System.out.println("Nos vemos pronto");
+					break;
+				default:
+				System.out.println("Opcion invalida, prueba con otra");	
+					break;
+			}
+			
+		} while (opcion <5 || >=0);
 	}
 	public void registro(){
 		Reserva r = new Reserva();
@@ -33,11 +63,11 @@ public class Consola {
 		Terraza t = new Terraza();
 		ArrayList<Reserva> nuevaReserva = new ArrayList<>();
 		
-		i.setNombre(pideDato("Diem el nombre: "));
-		i.setApellido1(pideDato("Dime el primer apellido: "));
-		i.setApellido2(pideDato("Dide el segundo apellido: "));
-		i.setCorreo(pideDato("DEime el correo: "));
-		i.setDireccion(pideDato("Dime la dirreccion: "));
+		i.setNombre(pideCadena("Nombre: "));
+		i.setApellido1(pideCadena("Primer apellido: "));
+		i.setApellido2(pideCadena("Segundo apellido: "));
+		i.setCorreo(pideCadena("Correo electronico: "));
+		i.setDireccion(pideCadena("Direccion: "));
 		
 		
 		
